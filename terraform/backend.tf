824bcc452a17f8b8af13facf455f8fb3dd902f80
Fabricio Valdivia <walter.valdivia7685242@gmail.com>
2026-02-11 18:46:57 -0500

    pipeline-terraform-cicd-kubernetes aws 4

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "project03-terraform-state"
+    bucket         = "project03-terraform-state12345"
     key            = "eks/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "project03-terraform-lock"
